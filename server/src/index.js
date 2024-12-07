238f50429a31a3c631b32b1ab3a75d3bd2281d7a
rlmgtr <rlmagtira@gmail.com>
2024-12-07 17:10:45 +0800

    First Postman test - working

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,6 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const cors = require('cors');
-
-const router = require('./router');
-
-dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(morgan('tiny'));
-app.use(router); // since index is on the json, this line is to call all the routes in the router.js file.
 
 app.listen(8000);
-
-
