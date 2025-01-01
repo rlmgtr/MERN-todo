3fc3bcc8f31a642d85dfc7e4d1d0b8bd6a6c2450
rlmgtr <rlmagtira@gmail.com>
2025-01-02 05:55:53 +0800

    Done with auth - will review

--- server/src/middleware/isLoggedIn.js
+++ server/src/middleware/isLoggedIn.js
@@ -1,11 +1,18 @@
+const jwt = require('jsonwebtoken');
+
 module.exports = (req, res, next) => {
 const authHEader = req.headers.authorization;
 if (!authHEader) {
     res.status(401).send('Please log in to continue');
 } else {
     const token = authHEader.split(' ')[1];
+    console.log(token);
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            res.status(403).send('invalid log in');
+        } else {
+            next();
+        } 
+    });
 }
-
-
-
 }
--- server/src/router.js
+++ server/src/router.js
@@ -1,13 +1,20 @@
 const express = require('express');
 
+const isLoggedIn = require ('./middleware/isLoggedIn.js')
+
 const router = express.Router();
 
 router.post('/login', require('./routes/loginRoute'));
 
-router.get('/todo', require('./routes/todoRoute'));
+router.get('/todo', isLoggedIn, require('./routes/todoRoute'));
 
 module.exports = router;
 
+
+
+
+
+
 // https://www.youtube.com/watch?v=oJBu2k7OEk8&t=1773s
-// 35:00
-// you are now putting the middleware so auth will be applicable to pages
+// 41:00
+// now we are setting up mdb
