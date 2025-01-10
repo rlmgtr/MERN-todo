f56d1f4009787dc9dc3e0f516fd1cef077ed1df3
rlmgtr <rlmagtira@gmail.com>
2025-01-10 21:59:51 +0800

    starting client directory

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+useEffect(() => {
+console.log('this is the useEffect')
+
+}, []);
+
+    return (
+    <div className="App">
+      This is just plain
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+// https://www.youtube.com/watch?v=oJBu2k7OEk8&t=1773s
+// 1: 11
