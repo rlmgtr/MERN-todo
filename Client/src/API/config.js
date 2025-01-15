3fb6c40f191c151c64437c8d2a421527bc0893f5
rlmgtr <rlmagtira@gmail.com>
2025-01-15 16:58:50 +0800

    update in schema and components

--- Client/src/API/config.js
+++ Client/src/API/config.js
@@ -1,3 +1,3 @@
 export const API_URL = `https://mern-todo-9ieq.onrender.com`; 
 
-export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTczNjkyNzk5MX0.fUMXEAc_yzeej93R7pV_GoS3AslWYUQnd1EeCzNvg7c';
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTczNjkzMDg5MH0.5SotO8zCw_oEvsIko-Fqthv2R9d7guo6_ICjAcgY2kU';
--- server/src/models/toDoModel.js
+++ server/src/models/toDoModel.js
@@ -1,56 +1,16 @@
-import React from 'react'
-import { useQueryClient, useMutation, QueryClient } from 'react-query'
-import updateToDoRequest from '../API/updateToDoRequest';
+const mongoose = require('mongoose');
 
-const TodoItem = ({ todo }) => {
-const queryClient = useQueryClient();
-  const {mutate: toggleCompletion} = useMutation(
-    (updateToDo) => {
-return updateToDoRequest(updateToDo);
-  }, {
-    onSettled: () => {
-      queryClient.invalidateQueries('todos');
+const toDoSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+      completed: {
+        type: Boolean,
+        default: false, // Set a default value
+      },
     },
-  }
-);
-
-const updateText = (e) => {
-
-}
-
-//////
-  return (
-    <div>
-      <input  
-      checked={todo.completed} 
-      type='checkbox' 
-      onChange={() => updateToDo({
-        ...todo,
-        completed: !todo.completed, 
-      })}
-      />
-
-    
-
-
-      <input  
-      checked={todo.text} 
-      type='text' 
-      value={todo.text}
-      onChange={(e) => updateToDo({
-        ...todo,
-        text:e.target.value,
-      })
-
-      }
-      />
-
-
-    </div>
+    { versionKey: false } // Optional: Disable the __v field
   );
-};
-
-export default TodoItem
-
-
-// https://www.youtube.com/watch?v=oJBu2k7OEk8&t=1773s
+  
