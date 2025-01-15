73107c8e008d2af0cc704a9f1295cd64e93a74aa
rlmgtr <rlmagtira@gmail.com>
2025-01-15 16:38:08 +0800

    interface test

--- Client/src/Components/TodoItem.jsx
+++ Client/src/Components/TodoItem.jsx
@@ -1,51 +1,56 @@
-import React from 'react';
-import { useQueryClient, useMutation } from 'react-query';
+import React from 'react'
+import { useQueryClient, useMutation, QueryClient } from 'react-query'
 import updateToDoRequest from '../API/updateToDoRequest';
 
 const TodoItem = ({ todo }) => {
-  const queryClient = useQueryClient();
-
-  const { mutate: toggleCompletion } = useMutation(
-    async (updatedToDo) => {
-      console.log('Updating TODO:', updatedToDo);
-      return await updateToDoRequest(updatedToDo);
+const queryClient = useQueryClient();
+  const {mutate: toggleCompletion} = useMutation(
+    (updateToDo) => {
+return updateToDoRequest(updateToDo);
+  }, {
+    onSettled: () => {
+      queryClient.invalidateQueries('todos');
     },
-    {
-      onError: (error) => {
-        console.error('Error updating TODO:', error.message);
-      },
-      onSettled: () => {
-        queryClient.invalidateQueries('todos');
-      },
-    }
-  );
+  }
+);
 
-  const handleTextChange = (newText) => {
-    toggleCompletion({
-      ...todo,
-      text: newText,
-    });
-  };
+const updateText = (e) => {
 
+}
+
+//////
   return (
     <div>
-      <input
-        checked={todo.completed}
-        type="checkbox"
-        onChange={() =>
-          toggleCompletion({
-            ...todo,
-            completed: !todo.completed,
-          })
-        }
+      <input  
+      checked={todo.completed} 
+      type='checkbox' 
+      onChange={() => updateToDo({
+        ...todo,
+        completed: !todo.completed, 
+      })}
       />
-      <input
-        type="text"
-        value={todo.text}
-        onChange={(e) => handleTextChange(e.target.value)}
+
+    
+
+
+      <input  
+      checked={todo.text} 
+      type='text' 
+      value={todo.text}
+      onChange={(e) => updateToDo({
+        ...todo,
+        text:e.target.value,
+      })
+
+      }
       />
+
+
     </div>
   );
 };
 
-export default TodoItem;
+export default TodoItem
+
+
+// https://www.youtube.com/watch?v=oJBu2k7OEk8&t=1773s
