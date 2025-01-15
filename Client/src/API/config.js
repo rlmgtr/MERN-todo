a0befdb5aefe43b26fece4d184358d35fd87e605
rlmgtr <rlmagtira@gmail.com>
2025-01-15 22:22:55 +0800

    adding GH pages

--- Client/src/API/config.js
+++ Client/src/API/config.js
@@ -1,3 +1,3 @@
-export const API_URL = `https://mern-todo-9ieq.onrender.com`; 
+export const API_URL = `http://localhost:8000`; 
 
 export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEsImlhdCI6MTczNjkzMDg5MH0.5SotO8zCw_oEvsIko-Fqthv2R9d7guo6_ICjAcgY2kU';
